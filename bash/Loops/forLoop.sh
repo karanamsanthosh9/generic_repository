1773c1eb9e37bb3bca24313b7dfdd1b8ebf576a1
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-01-07 17:15:45 +0000

    for loops

--- bash/Loops/forLoop.sh
+++ bash/Loops/forLoop.sh
@@ -2,20 +2,60 @@
 
 echo "For Loop in Loops concept"
 
-<< --
-for var in {1..100}
-do
-	echo $var
-done
---
+function model1
+{
+	for var in {1..100}
+	do
+		echo $var
+	done
+}
 
-arr=( fsfs 12  f..13 13108 ooo )
+function model2
+{
+	arr=( fsfs 12  f..13 13108 ooo )
+	echo size of arr is ${#arr[*]}
+	echo ${arr[0]}
+	echo ${arr[1]}
 
-echo size of arr is ${#arr[*]}
-echo ${arr[0]}
-echo ${arr[1]}
+	for var in ${arr[@]}
+	do 
+		echo $var
+	done
+}
 
-for var in ${arr[@]}
-do 
-	echo $var
-done
+function model3
+{
+
+	arr=( fsfs 12  f..13 13108 ooo )
+	echo size of arr is ${#arr[*]}
+	echo Printing array elements in single row
+	echo ${arr[*]}
+
+}
+
+function model4
+{
+	Nine_multiples=( 9 18 27 36 45 54 63 72 81 90 )
+	for((i=0;i<${#Nine_multiples[*]};i++))
+	do 
+		#echo 9 * $((i+1)) = ${Nine_multiples[$i]} that * is printing filename(s) in current dir🙂
+		echo 9 "*" $((i+1)) = ${Nine_multiples[$i]}
+	done
+
+}
+
+function tables
+{
+
+	for var in {1..10}
+	do
+		echo ----------Printing $var table -------------- 
+		for v2 in {1..10}
+		do 
+			echo $var "*" $v2 = $((var*v2))
+		done 
+		
+	done
+}
+
+tables;
