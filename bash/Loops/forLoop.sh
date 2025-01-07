98ee48b8df2eb641c6d661891a1f62bb5b555259
karanamsanthosh9 <143432611+karanamsanthosh9@users.noreply.github.com>
2025-01-07 16:54:28 +0000

    for

--- bash/Loops/forLoop.sh
+++ bash/Loops/forLoop.sh
@@ -2,7 +2,20 @@
 
 echo "For Loop in Loops concept"
 
+<< --
 for var in {1..100}
 do
-echo $var
+	echo $var
+done
+--
+
+arr=( fsfs 12  f..13 13108 ooo )
+
+echo size of arr is ${#arr[*]}
+echo ${arr[0]}
+echo ${arr[1]}
+
+for var in ${arr[@]}
+do 
+	echo $var
 done
